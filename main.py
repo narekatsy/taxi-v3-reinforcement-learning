4a8ca41057afde80a76285a4b03cd902521d618f
narekatsy <narek.hakobyan.010803@gmail.com>
2025-05-10 23:15:53 +0400

    updated main.py

--- main.py
+++ main.py
@@ -17,6 +17,35 @@ def run_training(algo, episodes):
             "--episodes", str(episodes),
             "--verbose"
         ])
+    elif algo == "expected_sarsa":
+        subprocess.run([
+            sys.executable, "-m", "train.train_expected_sarsa",
+            "--config", "configs/expected_sarsa.json",
+            "--episodes", str(episodes),
+            "--verbose"
+    ])
+    elif algo == "monte_carlo":
+        subprocess.run([
+            sys.executable, "-m", "train.train_monte_carlo",
+            "--config", "configs/monte_carlo.json",
+            "--episodes", str(episodes),
+            "--verbose"
+    ])
+    elif algo == "n_step_sarsa":
+        subprocess.run([
+            sys.executable, "-m", "train.train_n_step_sarsa",
+            "--config", "configs/n_step_sarsa.json",
+            "--episodes", str(episodes),
+            "--verbose"
+    ])
+    elif algo == "dyna_q":
+        subprocess.run([
+            sys.executable, "-m", "train.train_dyna_q",
+            "--config", "configs/dyna_q.json",
+            "--episodes", str(episodes),
+            "--verbose"
+    ])
+
     else:
         print(f"Unknown algorithm: {algo}")
 
@@ -25,14 +54,23 @@ def run_evaluation(algo):
         subprocess.run([sys.executable, "-m", "evaluate.evaluate_q_learning"])
     elif algo == "sarsa":
         subprocess.run([sys.executable, "-m", "evaluate.evaluate_sarsa"])
+    elif algo == "expected_sarsa":
+        subprocess.run([sys.executable, "-m", "evaluate.evaluate_expected_sarsa"])
+    elif algo == "monte_carlo":
+        subprocess.run([sys.executable, "-m", "evaluate.evaluate_monte_carlo"])
+    elif algo == "n_step_sarsa":
+        subprocess.run([sys.executable, "-m", "evaluate.evaluate_n_step_sarsa"])
+    elif algo == "dyna_q":
+        subprocess.run([sys.executable, "-m", "evaluate.evaluate_dyna_q"])
     else:
         print(f"Unknown algorithm: {algo}")
 
 def main():
     parser = argparse.ArgumentParser(description="Main runner for RL project")
-    parser.add_argument("--algo", choices=["q_learning", "sarsa"], required=True, help="Algorithm to run")
+    parser.add_argument("--algo", choices=["q_learning", "sarsa", "expected_sarsa", "monte_carlo", "n_step_sarsa", "dyna_q"],
+                        required=True, help="Algorithm to run")
     parser.add_argument("--mode", choices=["train", "evaluate", "all"], default="all", help="Run mode")
-    parser.add_argument("--episodes", type=int, default=5000, help="Number of episodes (for training)")
+    parser.add_argument("--episodes", type=int, default=3000, help="Number of episodes (for training)")
     args = parser.parse_args()
 
     if args.mode in ("train", "all"):
